024203c96ba88833251727ce8c483ad090106fc7
Oleg Drokin <green@linuxhacker.ru>
2019-02-03 20:44:27 -0500

    builder script: also accept the machine name to be passed to systemd-nspawn

--- builder-run.sh
+++ builder-run.sh
@@ -2,4 +2,4 @@
 
 # Add parsing of parameters
 
-exec systemd-nspawn -q --read-only --bind=$1:/tmp/out --bind-ro=/home/green/build-and-test/bin-x86:/home/green/bin --tmpfs=/home/green/git/lustre-release:mode=777,size=3G -D /exports/centos7-base -u $4 /home/green/bin/run_build.sh $2 $3
+exec systemd-nspawn -M "$5" -q --read-only --bind=$1:/tmp/out --bind-ro=/home/green/build-and-test/bin-x86:/home/green/bin --tmpfs=/home/green/git/lustre-release:mode=777,size=3G -D /exports/centos7-base -u $4 /home/green/bin/run_build.sh $2 $3
