fdf996c7e1635b9e7081fbb81a8acbd374c447a4
Oleg Drokin <green@linuxhacker.ru>
2019-02-24 17:21:22 -0500

    SQL schema to the crashes db

---
+++ schema.sql
@@ -0,0 +1,6 @@
+create table known_crashes (id serial unique, reason varchar(120) NOT NULL, func varchar(50), testline varchar(120), backtrace text NOT NULL, inlogs varchar(200), infullbt varchar(200), bug varchar(20) NOT NULL, extrainfo text);
+create table new_crashes (id serial PRIMARY KEY, reason varchar(120) NOT NULL, func varchar(50), backtrace text NOT NULL); -- I wanter primary key on reason,func,backtrace but it does not work with select group by
+create table triage (id serial PRIMARY KEY, link varchar(200) NOT NULL, testline varchar(120), fullcrash text NOT NULL, testlogs text, newcrash_id integer not null references new_crashes (id) ON DELETE CASCADE, FOREIGN KEY (newcrash_id) references new_crashes (id)); -- blob with entire compressed dmesg?
+create index on known_crashes (reason, func, testline);
+create index on new_crashes (reason, func);
+
