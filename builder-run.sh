7c2116917c0420fa4633cd9bb130165046146b8a
Oleg Drokin <green@linuxhacker.ru>
2019-06-06 17:13:33 -0400

    build scripts: add basic other distros support
    just instroduce rhel8 for now

--- builder-run.sh
+++ builder-run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# arguments: outdir ref buildnr userid uniqprocessname
+
+DISTRO=${DISTRO:-"centos7"}
+
+ROOTDIR="/exports/${DISTRO}-base"
+if [ ! -d ${ROOTDIR} -o ! -d distros/${DISTRO} ] ; then
+	echo "Wrong distro $DISTRO"
+	exit 1
+fi
+
 # Add parsing of parameters
 
 # XXX - we run the builder at the lowest priority so that it does not disrupt
 # parallel VMs
-exec nice -n 10 systemd-nspawn -M "$5" -q --read-only --bind=$1:/tmp/out --bind-ro=/home/green/build-and-test/bin-x86:/home/green/bin --tmpfs=/home/green/git/lustre-release:mode=777,size=3G -D /exports/centos7-base -u $4 /home/green/bin/run_build.sh $2 $3
+exec nice -n 10 systemd-nspawn -M "$5" -q --read-only --bind=$1:/tmp/out --bind-ro=/home/green/build-and-test/distros/${DISTRO}/bin-x86:/home/green/bin --tmpfs=/home/green/git/lustre-release:mode=777,size=3G -D ${ROOTDIR} -u $4 /home/green/bin/run_build.sh $2 $3
