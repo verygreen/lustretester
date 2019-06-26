44db3141789981b095b360f330baf20e07c0f966
Oleg Drokin <green@linuxhacker.ru>
2019-06-25 22:20:42 -0400

    buildscript - have shared git location
    and ability oto override it via env.

--- builder-run.sh
+++ builder-run.sh
@@ -3,6 +3,7 @@
 # arguments: outdir ref buildnr userid uniqprocessname
 
 DISTRO=${DISTRO:-"centos7"}
+GITSOURCE=${GITSOURCE:-"/home/green/git/lustre-release"}
 
 ROOTDIR="/exports/${DISTRO}-base"
 if [ ! -d ${ROOTDIR} -o ! -d distros/${DISTRO} ] ; then
@@ -14,4 +15,8 @@ fi
 
 # XXX - we run the builder at the lowest priority so that it does not disrupt
 # parallel VMs
-exec nice -n 10 systemd-nspawn -M "$5" -q --read-only --bind=$1:/tmp/out --bind-ro=/home/green/build-and-test/distros/${DISTRO}/bin-x86:/home/green/bin --tmpfs=/home/green/git/lustre-release:mode=777,size=3G -D ${ROOTDIR} -u $4 /home/green/bin/run_build.sh $2 $3
+exec nice -n 10 systemd-nspawn -M "$5" -q --read-only --bind=$1:/tmp/out \
+	--bind-ro=/home/green/build-and-test/distros/${DISTRO}/bin-x86:/home/green/bin \
+	--bind-ro=${GITSOURCE}:/home/green/git/lustre-release-base \
+	--tmpfs=/home/green/git/lustre-release:mode=777,size=3G -D ${ROOTDIR} \
+	-u $4 /home/green/bin/run_build.sh $2 $3
