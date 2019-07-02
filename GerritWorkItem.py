f38f3797138e07db0a8aad32d0ff7d4fffa3aa3d
Oleg Drokin <green@linuxhacker.ru>
2019-07-02 16:55:23 -0400

    workitem: catch exception from print if item is too big to fit the output buffer

--- GerritWorkItem.py
+++ GerritWorkItem.py
@@ -200,7 +200,12 @@ class GerritWorkItem(object):
                 else:
                     item["Warnings"] = Warnings
 
-        print("Build " + str(self.buildnr) + " Updated test element " + str(item))
+        try:
+            print("Build " + str(self.buildnr) + " Updated test element " + str(item))
+        except BlockingIOError:
+            # Sometimes the print is so big we cannot really output it in one go?
+            print("Build " + str(self.buildnr) + " Updated test element, but output too long!")
+
         sys.stdout.flush() # Make sure it's visible in its entirety over a pipe
         self.Write_HTML_Status()
 
