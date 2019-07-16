d75c13e5a325d0249f3981bd2716d9231cd41708
Oleg Drokin <green@linuxhacker.ru>
2019-07-16 12:10:45 -0400

    flush can also block it looks like

--- GerritWorkItem.py
+++ GerritWorkItem.py
@@ -206,11 +206,11 @@ class GerritWorkItem(object):
 
         try:
             print("Build " + str(self.buildnr) + " Updated test element " + str(item))
+            sys.stdout.flush() # Make sure it's visible in its entirety over a pipe
         except BlockingIOError:
             # Sometimes the print is so big we cannot really output it in one go?
             print("Build " + str(self.buildnr) + " Updated test element, but output too long!")
 
-        sys.stdout.flush() # Make sure it's visible in its entirety over a pipe
         self.Write_HTML_Status()
 
         if Finished:
