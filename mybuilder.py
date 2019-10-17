3a2fac2723dc84ed333769f09af98b0402036fe3
Oleg Drokin <green@linuxhacker.ru>
2019-10-17 02:29:05 -0400

    builder: handle ldlm build errors properly
    since the file path is prefixed with lustre/ptlrpc/../.. - just
    strip that away.

--- mybuilder.py
+++ mybuilder.py
@@ -109,7 +109,7 @@ class Builder(object):
             if len(tmp) != 3:
                 continue
 
-            path = tmp[0].strip()
+            path = tmp[0].strip().replace('lustre/ptlrpc/../../','') # also strip ptlrpc/ldlm cruft
             lineno_str = tmp[1].strip()
             if not lineno_str.isdigit():
                 continue
